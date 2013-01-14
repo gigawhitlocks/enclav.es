3290478e168d823bcee028586626e8b28662cc11
Ian Whitlock <ian@thewhitlockian.com>
2013-01-14 01:52:18 -0500

    initial setup

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to the-known-net
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject the-known-net "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.4.0"]
+			[ring "1.1.7"]])
