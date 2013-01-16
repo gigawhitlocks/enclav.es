32c632a41a9debed50da09462de7f4a1895f82e9
Ian Whitlock <ian@thewhitlockian.com>
2013-01-16 03:31:01 -0500

    built an example adder from a tutorial. had to make some changes because my software was newer than the original but got it working :D

--- project.clj
+++ project.clj
@@ -1,9 +1,14 @@
-(defproject the-known-net "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojure-contrib "1.2.0"]
-		 [ring/ring-core "0.3.7"]
-                 [ring/ring-jetty-adapter "0.3.7"]])
+(defproject adder "0.0.1"
+    :description "Add two numbers."
+    :dependencies
+      [[org.clojure/clojure "1.4.0"]
+            [org.clojure/clojure-contrib "1.2.0"]
+            [ring/ring-core "0.2.5"]
+            [ring/ring-devel "0.2.5"]
+            [ring/ring-jetty-adapter "0.2.5"]
+            [compojure "0.4.0"]
+            [hiccup "0.2.6"]]
+    :dev-dependencies
+      [[lein-run "1.0.0-SNAPSHOT"]])
+
+
