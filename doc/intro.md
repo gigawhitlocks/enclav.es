126dcc2082efadd9e5fc4e8f7427e9c789ea28e8
Ian Whitlock <ian@thewhitlockian.com>
2013-01-19 18:30:53 -0500

    too much housekeeping lol

--- doc/intro.md
+++
@@ -1,3 +0,0 @@
-# Introduction to the-known-net
-
-TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
--- test/the_known_net/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns the-known-net.core-test
-  (:use clojure.test
-        the-known-net.core))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
