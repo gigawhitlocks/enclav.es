ee0af1693b465831f090ee654165116b91829818
Viraj Sinha <synhapse@gmail.com>
2013-01-20 19:34:11 -0500

    removed a couple commented old versions of repos

--- project.clj
+++ project.clj
@@ -3,13 +3,11 @@
     :dependencies
       [[org.clojure/clojure "1.4.0"]
             [ring "1.1.8"]
-           ; [ring/ring-devel "0.2.5"]
-           ; [ring/ring-jetty-adapter "0.2.5"]
-           ; [compojure "0.4.0"]
-          ;  [hiccup "0.2.6"]]
-          [hiccup "1.0.2"]
-          [sandbar/sandbar "0.4.0-SNAPSHOT"]
-           [compojure "1.1.5"]]
+          ; [ring/ring-devel "0.2.5"]
+          ; [ring/ring-jetty-adapter "0.2.5"]
+            [hiccup "1.0.2"]
+            [sandbar/sandbar "0.4.0-SNAPSHOT"]
+            [compojure "1.1.5"]]
     :dev-dependencies
       [[lein-run "1.0.0-SNAPSHOT"]]
     :main the-known-net.core)
