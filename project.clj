711595cc09be1330b6a375d89cd4430f8b35c635
Ian <unknown.mosquito@gmail.com>
2013-01-21 00:11:02 -0500

    added more deps

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
     :dependencies
       [[org.clojure/clojure "1.4.0"]
             [ring "1.1.8"]
-          ; [ring/ring-devel "0.2.5"]
-          ; [ring/ring-jetty-adapter "0.2.5"]
             [hiccup "1.0.2"]
             [sandbar/sandbar "0.4.0-SNAPSHOT"]
-            [compojure "1.1.5"]]
+            [compojure "1.1.5"]
+            [korma "0.3.0-RC2"] 
+            [cssgen "0.2.6"]]
     :dev-dependencies
       [[lein-run "1.0.0-SNAPSHOT"]]
     :main the-known-net.core)
