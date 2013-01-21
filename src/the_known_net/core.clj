87ff79051e535bffd6438029524502d3ef6b83be
Ian Whitlock <ian@thewhitlockian.com>
2013-01-21 03:04:47 -0500

    removed remaining non-original code

--- src/the_known_net/core.clj
+++ src/the_known_net/core.clj
@@ -1,65 +1,37 @@
 (ns the-known-net.core
     (:use hiccup.core)
     (:use hiccup.page)
-    (:use hiccup.form)
     (:use ring.adapter.jetty)
     (:use compojure.core)
     (:require [compojure.route :as route]))
 
-(defn view-layout [& content]
+;Generates HTML header and shit
+(defn view-layout [ title & content]
     (html (xhtml-tag "en"
           [:head
                  [:meta {:http-equiv "Content-type"
                          :content "text/html; charset=utf-8"}]
-                 [:title "theknown.net"]
+                 [:title ( str "theknown.net" title )]
+                 [:script {:src "http://use.edgefonts.net/quattrocento-sans.js"}]
                  [:link {:href "http://theknown.net/main.css" :type "text/css" :rel "stylesheet"} ]]
           [:body content])))
 
-(defn view-input []
-    (view-layout
-          [:h2 "add two numbers"]
-          [:form {:method "post" :action "/"}
-                 [:input.math {:type "text" :name "a"}] [:span.math " + "]
-                 [:input.math {:type "text" :name "b"}] [:br]
-                 [:input.action {:type "submit" :value "add"}]]))
 
-(defn view-output [a b sum]
-    (view-layout
-          [:h2 "two numbers added"]
-          [:p.math a " + " b " = " sum]
-          [:a.action {:href "/"} "add more numbers"]))
-
-(defn parse-input [a b]
-    [(Integer/parseInt a) (Integer/parseInt b)])
-
+;Generates the site's landing page
 (defn landing-page [] 
-  (view-layout
-      [:h1 "theknown.net"]
-      [:h2 "is invite only."]
+  (view-layout "" ; first arg is page title
+      [:h1 {:class "isinviteonly"} "theknown.net "]
+      [:h2 {:class "isinviteonly"} "is invite only."]
       [:br][:a {:style "position:absolute; bottom:14%" :href "sign-in"} "I have an account or an invitation."]))
 
-(defn signup []
-  [:div 
-   [:form {:method "post" :action "/sign-up"}
-    [:br][:input.text]]])
 
-(defn signin-page [] 
-  (view-layout
-          (landing-page)
-          (signup)))
-
-
+;Routes
 (defroutes app
     (GET "/" []
-             (landing-page))
-    (GET "/sign-in" [] 
-             (signin-page)))
+             (landing-page)))
+ ;   (GET "/sign-in" [] 
+  ;           (signin-page)))
 
-;    (POST "/" [a b]
-;              (let [[a b] (parse-input a b)
-;                              sum   (+ a b)]
-;                      (view-output a b sum))))
-
-;(defn -main [] (run-jetty {:port 1337}))
 (defn -main []
     (run-jetty #'the-known-net.core/app {:port 1337 :join? true}))
+
