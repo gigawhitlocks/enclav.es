c01ce346816a991168ca016d676de2d7837ec548
Ian Whitlock <ian@thewhitlockian.com>
2013-01-23 18:15:15 -0500

    added 404 page

--- src/the_known_net/core.clj
+++ src/the_known_net/core.clj
@@ -26,11 +26,18 @@
       [:br][:a {:style "position:absolute; bottom:14%" :href "sign-in"} "I have an account or an invitation."]))
 
 
+(defn notfounderror-page []
+  (view-layout ""
+    [:center
+     "Page not found."
+     [:br][:br]
+     [:iframe {:width 560 :height 315 :src "http://www.youtube.com/embed/kxopViU98Xo" :frameborder 0}]]))
+
 ;Routes
 (defroutes app
     (GET "/" []
              (landing-page))
-    (route/not-found (landing-page)))
+    (route/not-found (notfounderror-page))) ; TODO: IMPLEMENT 404 PAGE
  ;   (GET "/sign-in" [] 
   ;           (signin-page)))
 
