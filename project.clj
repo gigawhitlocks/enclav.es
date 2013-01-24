20d4c6a60b44df0be8363090f43c6bad8f0a5ad9
Viraj Sinha <synhapse@gmail.com>
2013-01-23 20:00:19 -0500

    develop without restarting the servergit add project.clj run 'lein ring server 1337' to start

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject the-known-net "0.0.1"
     :description "An experimental online meta-community."
+    :plugins [[lein-ring "0.8.2"]]
     :dependencies
       [[org.clojure/clojure "1.4.0"]
             [ring "1.1.8"]
@@ -10,6 +11,9 @@
             [cssgen "0.2.6"]]
     :dev-dependencies
       [[lein-run "1.0.0-SNAPSHOT"]]
+    :ring {:handler the-known-net.core/app
+           :auto-reload? true
+           :auto-refresh? true}
     :main the-known-net.core)
 
 
