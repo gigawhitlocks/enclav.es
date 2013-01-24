9033a52fcc84d79b2c0d0d594c4ec8d3731e99eb
Viraj Sinha <synhapse@gmail.com>
2013-01-24 15:56:15 -0500

    more jvm-opts

--- project.clj
+++ project.clj
@@ -4,14 +4,15 @@
     :dependencies
       [[org.clojure/clojure "1.4.0"]
             [ring "1.1.8"]
-            [hiccup "1.0.0"]
+            [ring/ring-devel "1.1.0"]
+            [hiccup "1.0.2"]
             [sandbar/sandbar "0.4.0-SNAPSHOT"]
             [compojure "1.1.5"]
             [korma "0.3.0-RC2"] 
             [clj-style "1.0.1"]]
     :dev-dependencies
       [[lein-run "1.0.0-SNAPSHOT"]
-       [ring/ring-devel "1.1.0"]]
+       [lein-diagnosics "0.0.1"]]
     :ring {:handler the-known-net.core/app
            :auto-reload? true
            :auto-refresh? true}
@@ -22,7 +23,7 @@
                "-XX:+CMSConcurrentMTEnabled"  ; enable muli-threaded concurrent GC work
                "-XX:MaxGCPauseMillis=20"      ; specify target of 20ms for max GC pauses
                "-XX:+CMSIncrementalMode"      ; do many small GC cycles to minimize pauses
+               "-XX:MaxNewSize=256m"
+               "-XX:NewSize=257m"
                ]
     :main the-known-net.core)
-
-
