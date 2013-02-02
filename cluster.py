ff341b9088fc86ff499cc2c1fc0d0cbf7a559e00
Viraj Sinha <vsinha@purdue.edu>
2013-02-02 17:15:08 -0500

    made Application its own class, added deps for tornado auth

--- cluster.py
+++ cluster.py
@@ -1,27 +1,34 @@
 # vim: set ts=4 sw=4 noet
 
+import tornado.auth
+import tornado.escape
 import tornado.httpserver
 import tornado.ioloop
 import tornado.options
 import tornado.web
 
 from jinja2 import Environment, FileSystemLoader
-env = Environment(loader=FileSystemLoader('templates'))
-template = env.get_template('landingpage.html')
-
 from tornado.options import define, options
+
+env = Environment(loader=FileSystemLoader('templates')) #load ./templates/
+template = env.get_template('landingpage.html')
 
 define("port", default=1337, help="run on the given port", type=int)
 
+class Application(tornado.web.Application):
+	def __init__(self):
+		handlers = [
+				(r"/", MainHandler),
+		]
+		tornado.web.Application.__init__(self, handlers)
+
 class MainHandler(tornado.web.RequestHandler):
 	def get(self):
-		self.write(template.render())
+		self.write("hello world!")
+		#self.write(template.render())
 
 def main():
-	application = tornado.web.Application([
-		(r"/", MainHandler),
-	])
-	http_server = tornado.httpserver.HTTPServer(application)
+	http_server = tornado.httpserver.HTTPServer(Application())
 	http_server.listen(options.port)
 	tornado.ioloop.IOLoop.instance().start()
 
