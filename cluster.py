fa0bc72fb067ef9e9b1ef971dd5ffc8cd3f38f95
Viraj Sinha <vsinha@purdue.edu>
2013-02-02 16:49:07 -0500

    PackageLoader was loading the file twice, refactored code for expansibility, everything works the exact same'

--- cluster.py
+++ cluster.py
@@ -1,20 +1,29 @@
+# vim: set ts=4 sw=4 noet
+
+import tornado.httpserver
 import tornado.ioloop
+import tornado.options
 import tornado.web
 
-from jinja2 import Environment, PackageLoader
-env = Environment(loader=PackageLoader('cluster', 'templates'))
+from jinja2 import Environment, FileSystemLoader
+env = Environment(loader=FileSystemLoader('templates'))
 template = env.get_template('landingpage.html')
 
+from tornado.options import define, options
+
+define("port", default=1337, help="run on the given port", type=int)
 
 class MainHandler(tornado.web.RequestHandler):
-	    def get(self):
-		            self.write(template.render())
+	def get(self):
+		self.write(template.render())
 
-application = tornado.web.Application([
-	    (r"/", MainHandler),
-])
+def main():
+	application = tornado.web.Application([
+		(r"/", MainHandler),
+	])
+	http_server = tornado.httpserver.HTTPServer(application)
+	http_server.listen(options.port)
+	tornado.ioloop.IOLoop.instance().start()
 
 if __name__ == "__main__":
-	    application.listen(1337)
-	    tornado.ioloop.IOLoop.instance().start()
-
+	main()
