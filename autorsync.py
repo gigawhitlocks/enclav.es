e6da4fa59d382574ce1e9d9425e2266c53691de9
Ian Whitlock <iwhitloc@purdue.edu>
2013-02-19 00:47:59 -0500

    commented the file

--- autorsync.py
+++ autorsync.py
@@ -1,5 +1,4 @@
 #!/usr/local/bin/python
-
 import sys
 import pyinotify
 from subprocess import call
@@ -7,38 +6,47 @@ import signal as sig
 from os import getcwd
 
 
+
+
+# print usage info if args are wrong
 if ( len(sys.argv) != 3 ) :
-	print("USAGE: ./autorsync LOCAL_DIRECTORY REMOTE_DIRECTORY")
+	print("USAGE: ./autorsync LOCAL_DIRECTORY REMOTE_DIRECTORY\nEXAMPLE: ./autorsync . upload_dir")
 	sys.exit(0)
 
+# handle relative paths, because apparently daemonized pyinotify thinks the cwd is / by default
 if ( sys.argv[1][0] != '/' ):
 	sys.argv[1] = getcwd()+"/"+sys.argv[1]
 
+# too lazy to write tilde expansion, so it just prints a warning if you try to use it
+# this is totally half-assed
 if ( sys.argv[1][0] == '~'):
 	print("Tilde shorthand is not implemented. Exiting...")
 	sys.exit(0)
 
+# actually call rsync when an event is called
 def on_loop(notifier):
 	call(["rsync","-davh",sys.argv[1],"miranda.theknown.net:~/"+sys.argv[2]])
 
+# handle sigterm so that the program shuts down gracefully when killed
 def handle_sigTERM():
 	sys.stdout.write("SIGTERM caught! Exiting...\n")
 	notifier.stop()
 	sys.exit(0)
 
+# register the SIGTERM handler
 sig.signal(sig.SIGTERM, handle_sigTERM)
+
+# set up the pyinotify stuff
 wm = pyinotify.WatchManager()
 notifier = pyinotify.Notifier(wm)
+
+# this only watches the current directory.
+# TODO: make this watch child directories also
 wm.add_watch(sys.argv[1], pyinotify.IN_CREATE | pyinotify.IN_DELETE | pyinotify.IN_MODIFY)
 
-# Notifier instance spawns a new process when daemonize is set to True. This
-# child process' PID is written to /tmp/pyinotify.pid (it also automatically
-# deletes it when it exits normally). If no custom pid_file is provided it
-# would write it more traditionally under /var/run/. Note that in both cases
-# the caller must ensure the pid file doesn't exist when this method is called
-# othewise it will raise an exception. /tmp/stdout.txt is used as stdout 
-# stream thus traces of events will be written in it. callback is the above 
-# function and will be called after each event loop.
+
+# run pyinotify's notify loop, daemonized.
+# note that rsync breaks the stdout redirection for some reason, could debug it if I never need it
 try:
     notifier.loop(daemonize=True, callback=on_loop,
                   pid_file='/tmp/autorsync.pid', stdout='/tmp/autorsync.txt')
