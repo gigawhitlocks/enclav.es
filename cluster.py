7e1102c888f18a20d20002b056e08b2924daa58f
Ian Whitlock <iwhitloc@purdue.edu>
2013-02-28 20:49:31 -0500

    in the middle of looking up an existing user on sign in

--- cluster.py
+++ cluster.py
@@ -15,6 +15,9 @@ from session import *
 import hashlib
 from random import random
 
+from bulbs.neo4jserver import Graph
+from users import User
+
 #load ./templates/
 env = Environment(loader=FileSystemLoader('templates')) 
 
@@ -35,6 +38,10 @@ class Application(tornado.web.Application):
 				(r"/sign-up", InviteHandler)
 		]
 		tornado.web.Application.__init__(self, handlers,**settings)
+
+		# Sets up the graph db
+		g = Graph()
+		g.add_proxy("users",User)
 
 class LandingPageHandler(tornado.web.RequestHandler):
 		
@@ -60,7 +67,14 @@ class LandingPageHandler(tornado.web.RequestHandler):
 	# handles POST requests sent to /
 	def post(self):
 		if (self.get_current_user() == None):
+			g = Graph()
 			self.set_header("Content-Type", "text/plain")
+			user = g.users.index.get_unique(name=self.get_argument("username")) 
+			if ( user == None ):
+				self.write("No such user exists\n")
+			else :
+				self.write(user.uid)
+				
 			self.write("You wrote " + self.get_argument("username"))
 			self.write("You wrote " + self.get_argument("password"))
 			## instead of raising an error, we should authenticate here
