ccdcda078c57f90fc0df23784df05fba299355f9
Ian Whitlock <iwhitloc@purdue.edu>
2013-04-29 00:02:48 -0400

    complete through neo4j. redis and nginx, and disable dev mode of apf are next

--- provision.sh
+++ provision.sh
@@ -16,12 +16,11 @@ wget -q http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm \
 
 # disable EPEL repo by default
 echo "Disabling EPEL by default.. (use with --enablerepo=epel)";
-cat /etc/yum.repos.d/epel.repo | sed -e s/enabled=1/enabled=0/g > epel2.repo \
-	&& mv -f epel2.repo /etc/yum.repos.d/epel.repo;
+sed -i.bak s/enabled=1/enabled=0/g /etc/yum.repos.d/epel.repo;
 
 # install pip
-echo "Installing PIP, screen, & Python deps..";
-yum install screen python-pip -y --enablerepo=epel;
+echo "Installing extra software from repos..";
+yum install screen python-pip lsof expect -y --enablerepo=epel;
 
 # install deps
 pip-python install tornado redis jinja2 bulbs pytz;
@@ -29,6 +28,43 @@ pip-python install tornado redis jinja2 bulbs pytz;
 # turns off selinux
 echo 0 > /selinux/enforce;
 
-# turns off the firewall TODO: REMOVE THIS AND SET UP THE FIREWALL FO REALZ
-/etc/init.d/iptables stop;
+echo "Installing & configuring firewall..";
+# install apf & configure firewall
+wget -q http://www.rfxn.com/downloads/apf-current.tar.gz;
+tar xf apf-current.tar.gz;
+cd apf-[0-9]*;
+./install.sh;
 
+# open port 1337
+sed -i.bak s/IG_TCP_CPORTS=\"22\"/IG_TCP_CPORTS=\"22,1337\"/g /etc/apf/conf.apf \
+		&& apf -r;
+
+cd ..;
+rm -f apf-current.tar.gz;
+
+echo "Downloading Java..";
+wget -q http://theknown.net/jdk-6u45-linux-x64-rpm.bin;
+
+echo "Installing Java..";
+chmod +x jdk-6u45-linux-x64-rpm.bin;
+./jdk-6u45-linux-x64-rpm.bin;
+
+echo "Downloading Neo4j..";
+wget -q http://theknown.net/neo4j-community-1.8.1-unix.tar.gz;
+
+echo "Installing Neo4j..";
+tar xf neo4j-community-1.8.1-unix.tar.gz;
+expect -c 'set timeout 20; spawn "./neo4j-community-1.8.1/bin/neo4j" install;\
+	expect "*should run Neo4j?*" { send "\r" };\
+	expect "*does not yet exist. Shall I create the account for you?*" { send "\r" };\
+	interact;';
+
+echo "Configuring system for Neo4j..";
+ulimit -n 40000;
+echo -e "\nneo4j   soft    nofile  40000\nneo4j   hard    nofile  40000\n" >> /etc/security/limits.conf
+echo -e "\nsession    required   pam_limits.so\n" >> /etc/pam.d/su
+
+echo "Starting neo4j...";
+./neo4j-community-1.8.1/bin/neo4j start &
+
+echo "All done!";
