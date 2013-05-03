f747d015d3cd3e7bf359800a4b01b1a923c84f27
Ian Whitlock <iwhitloc@purdue.edu>
2013-05-03 11:15:38 -0400

    Just syncing things. Still experiencing weird Neo4j behavior.

--- provision.sh
+++ provision.sh
@@ -50,21 +50,37 @@ chmod +x jdk-6u45-linux-x64-rpm.bin;
 ./jdk-6u45-linux-x64-rpm.bin;
 
 echo "Downloading Neo4j..";
-wget -q http://theknown.net/neo4j-community-1.8.1-unix.tar.gz;
+cd /opt;
+wget -q http://theknown.net/neo4j-community-1.8.2-unix.tar.gz;
 
 echo "Installing Neo4j..";
-tar xf neo4j-community-1.8.1-unix.tar.gz;
-expect -c 'set timeout 20; spawn "./neo4j-community-1.8.1/bin/neo4j" install;\
-	expect "*should run Neo4j?*" { send "\r" };\
-	expect "*does not yet exist. Shall I create the account for you?*" { send "\r" };\
+tar xf neo4j-community-1.8.2-unix.tar.gz;
+useradd neo4j;
+expect -c 'set timeout 20; spawn "/opt/neo4j-community-1.8.2/bin/neo4j install";\
+	expect "*should run Neo4j?*" { send "neo4j\r" };\
 	interact;';
 
-echo "Configuring system for Neo4j..";
 ulimit -n 40000;
-echo -e "\nneo4j   soft    nofile  40000\nneo4j   hard    nofile  40000\n" >> /etc/security/limits.conf
-echo -e "\nsession    required   pam_limits.so\n" >> /etc/pam.d/su
+echo -e "\nneo4j   soft    nofile  40000\nneo4j   hard    nofile  40000\n" >> /etc/security/limits.conf;
+echo -e "\nsession    required   pam_limits.so\n" >> /etc/pam.d/su;
 
 echo "Starting neo4j...";
-./neo4j-community-1.8.1/bin/neo4j start &
+chown -R neo4j.neo4j /opt/neo4j-community-1.8.2;
+su - neo4j -c "/opt/neo4j-community-1.8.2/bin/neo4j start";
+cd /home/vagrant/;
 
-echo "All done!";
+# Download redis
+
+echo "Installing redis..";
+wget -q http://theknown.net/redis-2.6.9.tar.gz;
+tar xf redis-[0-9]*;
+cd redis-[0-9]*;
+make;
+make install;
+sysctl vm.overcommit_memory=1;
+echo "vm.overcommit_memory = 1" >> /etc/sysctl.conf
+
+# set redis to run in background & then run redis
+sed -i.bak 's/daemonize no/daemonize yes/g' /home/vagrant/redis-[0-9]*/redis.conf
+/usr/local/bin/redis-server /home/vagrant/redis-[0-9]*/redis.conf &
+
