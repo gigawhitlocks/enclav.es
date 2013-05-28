efce602f2b159f02e49ef36124220c8ea11c2808
Ian Whitlock <ian@theknown.net>
2013-05-28 00:05:36 -0400

    vagrant appears to be working; switched to ubuntu

--- provision.sh
+++ provision.sh
@@ -2,29 +2,33 @@
 
 # update the system
 echo "Starting full system update...";
-yum update -y;
+#yum update -y;
+apt-get update -y -q;
+#apt-get upgrade -y -q;
+
 echo "Finished updating. Performing initial setup..";
 
 # symlink the project directory somewhere more convenient
 ln -s /vagrant/ /home/vagrant/cluster;
 
 # install EPEL
-echo "Installing EPEL..";
-wget -q http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm \
-	&& yum -y localinstall epel-release-6-8.noarch.rpm \
- 	&& rm -f epel-release-6-8.noarch.rpm;
+#echo "Installing EPEL..";
+#wget -q http://mirror.pnl.gov/epel/6/i386/epel-release-6-8.noarch.rpm \
+#	&& yum -y localinstall epel-release-6-8.noarch.rpm \
+# 	&& rm -f epel-release-6-8.noarch.rpm;
 
 # disable EPEL repo by default
-echo "Disabling EPEL by default.. (use with --enablerepo=epel)";
-sed -i.bak s/enabled=1/enabled=0/g /etc/yum.repos.d/epel.repo;
+#echo "Disabling EPEL by default.. (use with --enablerepo=epel)";
+#sed -i.bak s/enabled=1/enabled=0/g /etc/yum.repos.d/epel.repo;
 
+apt-get install python-pip screen expect -y -q;
 # install pip
-echo "Installing extra software from repos..";
-yum install screen python-pip lsof expect -y --enablerepo=epel;
+#echo "Installing extra software from repos..";
+#yum install screen python-pip lsof expect -y --enablerepo=epel;
 
 # install deps
 #pip-python install tornado redis jinja2 bulbs pytz;
-pip-python install -r /home/vagrant/cluster/requirements.txt
+pip install -r /home/vagrant/cluster/requirements.txt
 
 
 # turns off selinux
@@ -42,9 +46,9 @@ cd /opt;
 wget -q https://dl.dropboxusercontent.com/u/14943993/neo4j-community-1.8.1-unix.tar.gz;
 
 echo "Installing Neo4j..";
-tar xf neo4j-community-1.8.2-unix.tar.gz;
+tar xf neo4j-community-1.8.1-unix.tar.gz;
 useradd neo4j;
-expect -c 'set timeout -1; spawn /opt/neo4j-community-1.8.2/bin/neo4j install;\
+expect -c 'set timeout -1; spawn /opt/neo4j-community-1.8.1/bin/neo4j install;\
 	expect "*should run Neo4j?*" { send "neo4j\r" };\
 	interact;';
 
@@ -53,8 +57,8 @@ echo -e "\nneo4j   soft    nofile  40000\nneo4j   hard    nofile  40000\n" >> /e
 echo -e "\nsession    required   pam_limits.so\n" >> /etc/pam.d/su;
 
 echo "Starting neo4j...";
-chown -R neo4j.neo4j /opt/neo4j-community-1.8.2;
-/opt/neo4j-community-1.8.2/bin/neo4j start;
+chown -R neo4j.neo4j /opt/neo4j-community-1.8.1;
+/opt/neo4j-community-1.8.1/bin/neo4j start;
 cd /home/vagrant/;
 
 # Download redis
