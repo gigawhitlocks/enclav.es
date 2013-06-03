11725496168322f62ff12f1e27c8632933f9938b
Ian Whitlock <ian@theknown.net>
2013-06-03 18:27:38 -0400

    content.html -> base.html

--- cluster.py
+++ cluster.py
@@ -57,7 +57,7 @@ class LandingPageHandler(tornado.web.RequestHandler):
 		else:
 			## a user is logged in
 			self.set_header("Content-Type","text/html")
-			header_template = env.get_template('base.html')
+			header_template = env.get_template('content.html')
 			self.write(header_template.render())
 	#		self.write('Thank you for logging in, %s.' %self.get_current_user())
 
@@ -69,7 +69,7 @@ class LandingPageHandler(tornado.web.RequestHandler):
 	# handles POST requests sent to /
 	# Generally these are login requests
 	def post(self):
-		header_template = env.get_template('base.html')
+		header_template = env.get_template('content.html')
 		self.write(header_template.render())
 		if (self.get_current_user() == None):
 			# Sets up the graph db
