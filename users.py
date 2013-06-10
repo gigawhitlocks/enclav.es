79bad3f5504064e3d427fb1e0d23a4f2d9069f48
Ian Whitlock <ian@theknown.net>
2013-06-10 12:37:56 -0400

    cleaned out some stuff and added an Identity class and an Is relationship for handling identities.

--- users.py
+++ users.py
@@ -50,16 +50,34 @@ class User(Node):
 	password = String(nullable=False)
 	created = DateTime(default=current_datetime, nullable=False)
 
+
+# A User has many Identities. Identities are unqiue.
+# User --Is--> Identity
+class Identity(Node):
+	element_type = "identity"
+	identity = String(nullable=False)
+
+# nodes that exist before a user actually signs up but after invitation
 class Invitee(Node):
 	element_type = "invitee"
 	email = String(nullable=False)
 	token = String(nullable=False)
 	created = DateTime(default=current_datetime, nullable=False)
 
+# this relationship defines which Indentities belong to a User
+# User --Is--> Identity
+class Is(Relationship):
+	label = "is"
+	created = DateTime(default=current_datetime, nullable=False)
+
+# This relationship defines relationships between users
+# User --Follows--> User
 class Follows(Relationship):
 	label = "follows"
 	created = DateTime(default=current_datetime, nullable=False)
 
+# This relationship defines which user invited which user or invitee
+# User --Invited--> Invitee OR User --Invited--> User
 class Invited(Relationship):
 	label = "invited"
 	created = DateTime(default=current_datetime, nullable=False)
